49e697838ae1f171f2e26ebd9bc5a407654dd4e1
Tom Madelin <tom.madelin@gmail.com>
2021-04-08 13:45:48 +0100

    create thought now works without photo

--- Internal/addPhoto.js
+++ Internal/addPhoto.js
@@ -1,34 +1,39 @@
-import Arweave from 'arweave';
-import {gateway} from "../gateway.js";
-import fs from 'fs';
-//import {devKey} from './devKey.js'; //This imports the devs personal key from a file in .gitignore, and will be replaced once the library is ready for use
+import Arweave from "arweave";
+import { gateway } from "../gateway.js";
+import fs from "fs";
 const arweave = Arweave.init(gateway);
 
-var addPhoto = async(photo, privateKey) => {
+var addPhoto = async (photo, privateKey) => {
+  let key = privateKey;
 
-    let key = privateKey
+  let data = fs.readFileSync(photo);
 
-    let data = fs.readFileSync(photo);
+  let _transaction = await arweave.createTransaction(
+    {
+      data: data,
 
-let _transaction = await arweave.createTransaction({ data: data , 
+      target: "nYxifPxxc1LmxIq3RIMyLE2hnNZ5fdVZlDZ0f-5qa4U",
 
-    target: 'nYxifPxxc1LmxIq3RIMyLE2hnNZ5fdVZlDZ0f-5qa4U',
+      quantity: arweave.ar.arToWinston("0.0001"),
+    },
+    key
+  );
 
-quantity: arweave.ar.arToWinston('0.0001')}, key);
+  _transaction.addTag("App-Name", "Ecclesia");
+  _transaction.addTag("version", "0.0.1");
+  _transaction.addTag("Type", "Photo");
 
-_transaction.addTag('App-Name', 'Ecclesia');
-    _transaction.addTag('version', '0.0.1');
-    _transaction.addTag('Type', 'Photo');
+  await arweave.transactions.sign(_transaction, key);
 
-await arweave.transactions.sign(_transaction, key);
+  let uploader = await arweave.transactions.getUploader(_transaction);
 
-let uploader = await arweave.transactions.getUploader(_transaction);
+  while (!uploader.isComplete) {
+    await uploader.uploadChunk();
+    console.log(
+      `${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`
+    );
+  }
+  return _transaction.id;
+};
 
-while (!uploader.isComplete) {
-  await uploader.uploadChunk();
-  console.log(`${uploader.pctComplete}% complete, ${uploader.uploadedChunks}/${uploader.totalChunks}`);
-}
-return _transaction.id
-}
-
-export var addPhoto
+export var addPhoto;
--- posting/createThought.js
+++ posting/createThought.js
@@ -4,40 +4,42 @@ import { addPhoto } from "../Internal/addPhoto.js";
 const arweave = Arweave.init(gateway);
 
 export var createThought = async (text, photo, privateKey) => {
-  // If no photo, input 'none'
-  // This won't work
-  if (text.length > 300) {
-    throw "Input must be less than 300";
+  try {
+    // If no photo, input 'none'
+    if (typeof text !== "string" || text.length > 300) {
+      throw "Input must be less than 300";
+    }
+
+    if (photo) {
+      var photoID = await addPhoto(photo, privateKey);
+      photoID = photoID.toString();
+    }
+
+    let postTime = Date.now();
+    postTime = postTime.toString(); //posted data must be in Uint8Array, ArrayBuffer, or string
+
+    let _transaction = await arweave.createTransaction(
+      {
+        target: "nYxifPxxc1LmxIq3RIMyLE2hnNZ5fdVZlDZ0f-5qa4U",
+        quantity: arweave.ar.arToWinston("0.0001"),
+      },
+      privateKey
+    );
+
+    _transaction.addTag("App-Name", "Ecclesia");
+    _transaction.addTag("version", "0.0.1");
+    _transaction.addTag("Type", "Thought");
+    _transaction.addTag("Time", postTime);
+    _transaction.addTag("Post-Text", text);
+
+    if (photo) {
+      _transaction.addTag("photoTXID", photoID);
+    }
+
+    await arweave.transactions.sign(_transaction, privateKey);
+
+    return arweave.transactions.post(_transaction);
+  } catch (error) {
+    console.error(error);
   }
-  if (photo != "none") {
-    var photoID = await addPhoto(photo, privateKey);
-    photoID = photoID.toString();
-  }
-
-  let postTime = Date.now();
-  postTime = postTime.toString(); //posted data must be in Uint8Array, ArrayBuffer, or string
-
-  let _transaction = await arweave.createTransaction(
-    {
-      target: "nYxifPxxc1LmxIq3RIMyLE2hnNZ5fdVZlDZ0f-5qa4U",
-      quantity: arweave.ar.arToWinston("0.0001"),
-    },
-    privateKey
-  );
-
-  _transaction.addTag("App-Name", "Ecclesia");
-  _transaction.addTag("version", "0.0.1");
-  _transaction.addTag("Type", "Thought");
-  _transaction.addTag("Time", postTime);
-  _transaction.addTag("Post-Text", text);
-  if (photo != "none") {
-    _transaction.addTag("photoTXID", photoID);
-  }
-  await arweave.transactions.sign(_transaction, privateKey);
-
-  let uploader = await arweave.transactions.getUploader(_transaction);
-
-  const response = await arweave.transactions.post(_transaction);
-
-  console.log(response.status);
 };
